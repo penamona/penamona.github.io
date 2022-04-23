960d757f553be883de4f9c8426c7f746b6f97dc8
5mina <jtabrilla2025@clc.pshs.edu.ph>
2022-04-23 14:07:14 +0800

    love

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
-function loader() {
+function loader(y) {
+    if (y != undefined){
+        for (var x = 1; x <= y; x++) {
+            document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
+            document.getElementById("tip-"+x).style.transition="top 0.2s ease";
+        }
+    }
     const load = document.getElementById("load");
     load.style.opacity = 0;
     load.style.top = "100vh";
 }
 
 function tooltip(x) {
-    document.getElementById("tip-"+x).style.visibility="visible";
+    document.getElementById("tip-"+x).style.top="0px";
 }
 
 function tooltipH(x) {
-    document.getElementById("tip-"+x).style.visibility="hidden";
+    document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
 }

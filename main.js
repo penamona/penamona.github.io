2df8597f7b794881c6f66eb60231ffef36ba4a5e
5mina <jtabrilla2025@clc.pshs.edu.ph>
2022-04-24 17:00:18 +0800

    bio

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-function loader(y) {
-    if (y != undefined){
-        for (var x = 1; x <= y; x++) {
-            document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
-            document.getElementById("tip-"+x).style.opacity=0;
-            document.getElementById("tip-"+x).style.transition="top 0.2s ease, opacity 0.1s ease";
-        }
+var tip = [];
+
+function loader() {
+    var x = 1;
+    while (document.getElementById("tip-"+x) != null) {
+        document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
+        document.getElementById("tip-"+x).style.opacity=0;
+        document.getElementById("tip-"+x).style.transition="top 0.2s ease, opacity 0.1s ease";
+        tip[x] = false;
+        x++;
     }
     const load = document.getElementById("load");
     load.style.opacity = 0;
@@ -12,11 +15,13 @@ function loader(y) {
 }
 
 function tooltip(x) {
-    document.getElementById("tip-"+x).style.opacity=1;
-    document.getElementById("tip-"+x).style.top="0px";
-}
-
-function tooltipH(x) {
-    document.getElementById("tip-"+x).style.opacity=0;
-    document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
+    if (tip[x] == false) {
+        document.getElementById("tip-"+x).style.opacity=1;
+        document.getElementById("tip-"+x).style.top="0px";
+        tip[x] = true;
+    } else {
+        document.getElementById("tip-"+x).style.opacity=0;
+        document.getElementById("tip-"+x).style.top="-"+document.getElementById("tip-"+x).clientHeight+"px";
+        tip[x] = false;
+    }   
 }
